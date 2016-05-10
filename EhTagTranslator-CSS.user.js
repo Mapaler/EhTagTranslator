c45c3b6c12ae16ad4ba58776085e4f806c9ea82f
枫谷剑仙 <mapaler@163.com>
2016-05-10 19:11:17 +0800

    继续构建

--- EhTagTranslator-CSS.user.js
+++ EhTagTranslator-CSS.user.js
@@ -8,21 +8,37 @@
 // @copyright	2016+, Mapaler <mapaler@163.com>
 // ==/UserScript==
 
-var wiki_rows_URL="https://github.com/Mapaler/EhTagTranslator/wiki/rows"; //GitHub wiki 行名列表的地址
+var wiki_URL="https://github.com/Mapaler/EhTagTranslator/wiki"; //GitHub wiki 的地址
+var downOver; //检测下载是否完成的循环函数
+var rowsCount = 0; //行名总数
+var rowsCurrent = 0; //当前下载行名
 var ds = [];
-var row = function(){
+var rowObj = function(){
 	var obj = {
 		name:"",
 		cname:"",
+		info:{},
 		tags:[],
+		addTagFromName: function(rowObj)
+		{
+			if (rowObj == undefined)
+				rowObj = this;
+			GM_xmlhttpRequest({
+				method: "GET",
+				url: wiki_URL + "/" + this.name,
+				onload: function(response) {
+					dealTags(response.responseText,rowObj.tags);
+				}
+			});
+		},
 	}
 	return obj;
 }
-var tag = function(){
+var tagObj = function(){
 	var obj = {
 		name:"",
 		cname:"",
-		indo:"",
+		info:{},
 	}
 	return obj;
 }
@@ -47,24 +63,101 @@ if(typeof(GM_xmlhttpRequest) == "undefined")
 
 GM_xmlhttpRequest({
 	method: "GET",
-	url: wiki_rows_URL,
+	url: wiki_URL + "/rows",
 	onload: function(response) {
 		dealRows(response.responseText,ds);
 	}
 });
 
-//快速模式处理多图的回调函数
+//处理行的页面
 function dealRows(response, dataset)
 {
 	var parser = new DOMParser();
 	PageDOM = parser.parseFromString(response, "text/html");
 
-	var wiki_body = document.getElementsByClassName("wiki-body")[0];
+	var wiki_body = PageDOM.getElementById("wiki-body");
+	var table = wiki_body.getElementsByTagName("table")[0].tBodies[0];
+	rowsCount = table.rows.length;
+	for(var ri=0;ri<table.rows.length;ri++)
+	{
+		var trow = table.rows[ri];
+		var row = new rowObj;
+		row.name = trow.cells[0].textContent;
+		row.cname = trow.cells[1].textContent;
+		row.info = getTrueImgUrlInfo(trow.cells[2]);
+		row.addTagFromName();
+		dataset.push(row);
+	}
+	
+	clearInterval(downOver);
+	if (!startProgramCheck(dataset))
+	{
+		downOver = setInterval(function () { startProgramCheck(dataset) }, 500);
+	}
+}
+
+//获取介绍是图片还是文字
+function getTrueImgUrlInfo(dom)
+{
+	var info = {
+		type:0,
+		content:"",
+	};
+	if (dom.getElementsByTagName("img").length>0)
+	{
+		var img = dom.getElementsByTagName("img")[0];
+		info.type = 1;
+		info.content = img.getAttribute("data-canonical-src");
+	}
+	else
+	{
+		info.type = 0;
+		info.content = dom.textContent;
+	}
+	return info;
+}
+
+
+//处理Tag页面
+function dealTags(response, dataset)
+{
+	var parser = new DOMParser();
+	PageDOM = parser.parseFromString(response, "text/html");
+
+	var wiki_body = PageDOM.getElementById("wiki-body");
 	var table = wiki_body.getElementsByTagName("table")[0].tBodies[0];
 	
 	for(var ri=0;ri<table.rows.length;ri++)
 	{
-		var row = table.rows[ri];
-		console.log(row.cells[0].innerHTML);
+		var trow = table.rows[ri];
+		var tag = new tagObj;
+		tag.name = trow.cells[0].textContent;
+		tag.cname = trow.cells[1].textContent;
+		tag.info = getTrueImgUrlInfo(trow.cells[2]);
+		dataset.push(tag);
 	}
+	rowsCurrent++;
+}
+
+//检测下载完成情况
+function startProgramCheck(dataset)
+{
+	if (rowsCount > 0 && rowsCurrent >= rowsCount)
+	{
+		console.debug("获取完成");
+		clearInterval(downOver);
+		startCSSBuild(dataset);
+		return true;
+	}
+	else
+	{
+		console.debug("获取%d/%d",rowsCurrent, rowsCount);
+		return false;
+	}
+}
+
+//开始构建CSS
+function startCSSBuild(dataset)
+{
+	console.log(dataset);
 }
