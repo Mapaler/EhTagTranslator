e205f8031972d45cec9c61db5e76c23873566727
枫谷剑仙 <mapaler@163.com>
2016-05-11 11:21:23 +0800

    完成css建立函数

--- EhTagTranslator-CSS.user.js
+++ EhTagTranslator-CSS.user.js
@@ -9,6 +9,7 @@
 // ==/UserScript==
 
 var wiki_URL="https://github.com/Mapaler/EhTagTranslator/wiki"; //GitHub wiki 的地址
+var rows_title="rows"; //行名的地址
 var downOver; //检测下载是否完成的循环函数
 var rowsCount = 0; //行名总数
 var rowsCurrent = 0; //当前下载行名
@@ -25,7 +26,7 @@ var rowObj = function(){
 				rowObj = this;
 			GM_xmlhttpRequest({
 				method: "GET",
-				url: wiki_URL + "/" + this.name,
+				url: wiki_URL + (this.name.length?"/"+this.name:""),
 				onload: function(response) {
 					dealTags(response.responseText,rowObj.tags);
 				}
@@ -63,7 +64,7 @@ if(typeof(GM_xmlhttpRequest) == "undefined")
 
 GM_xmlhttpRequest({
 	method: "GET",
-	url: wiki_URL + "/rows",
+	url: wiki_URL + (rows_title.length?"/"+rows_title:""),
 	onload: function(response) {
 		dealRows(response.responseText,ds);
 	}
@@ -146,7 +147,8 @@ function startProgramCheck(dataset)
 	{
 		console.debug("获取完成");
 		clearInterval(downOver);
-		startCSSBuild(dataset);
+		var css = startCSSBuild(dataset);
+		console.log(css);
 		return true;
 	}
 	else
@@ -159,5 +161,68 @@ function startProgramCheck(dataset)
 //开始构建CSS
 function startCSSBuild(dataset)
 {
-	console.log(dataset);
+	var cssAry = [];
+	cssAry.push(""
+//▼CSS内容部分
+,"@namespace url(http://www.w3.org/1999/xhtml);"
+,""
+,"@-moz-document"
+,"    url-prefix('http://exhentai.org/g/'), "
+,"    url-prefix('http://g.e-hentai.org/g/')"
+,"{"
+,"  #taglist a:before{"
+,"    font-size:12px;"
+,"  }"
+,"  #taglist a:hover:after,#taglist a:focus:after{"
+,"    color:#fff;"
+,"    font-size:15px;"
+,"    background: #666;"
+,"    border: 1px solid #fff;"
+,"    border-radius:5px;"
+,"    float:left;"
+,"    position:fixed;"
+,"    top:25px;"
+,"    z-index:999;"
+,"    padding:2px;"
+,"    box-shadow: 0px 0px 5px #888888;"
+,"    min-width:150px;"
+,"    max-width:260px;"
+,"    white-space:pre-wrap;"
+,"    font-size:12px;"
+,"    transition: opacity 1s;"
+,"    opacity: 1;"
+,"  }"
+//▲CSS内容部分
+	)
+	
+	for (var ri = 0; ri < dataset.length; ri++)
+	{
+		var row = dataset[ri];
+		for (var ti = 0; ti < row.tags.length; ti++)
+		{
+			var tag = row.tags[ti];
+			cssAry.push(""
+//▼CSS内容部分
+,"  a[id|=\"ta_" + row.name + ":" + tag.name.replace(" ","_") + "\"]{"
+,"    font-size:0px;"
+,"  }"
+,"  a[id|=\"ta_" + row.name + ":" + tag.name.replace(" ","_") + "\"]:before{"
+,"    content:\"" + tag.cname + "\";"
+,"  }"
+,"  a[id|=\"ta_" + row.name + ":" + tag.name.replace(" ","_") + "\"]:hover:after,a[id|=\"ta_" + row.name + ":" + tag.name.replace(" ","_") + "\"]:focus:after{"
+,"    content:" + (tag.info.type?"url(" + tag.info.content + ")":"\"" + tag.info.content +　"\"") + ";"
+,"  }"
+//▲CSS内容部分
+			)
+		}
+	}
+	
+	cssAry.push(""
+//▼CSS内容部分
+,"}"
+//▲CSS内容部分
+	)
+	
+	var css = cssAry.join("\r\n");
+	return css;
 }
