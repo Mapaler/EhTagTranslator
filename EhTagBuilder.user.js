18801d86a671fd1fe92411745f8baef0aa233c67
枫谷剑仙 <mapaler@163.com>
2017-05-31 19:10:35 +0800

    1、更新仿GM_xmlhttpRequest函数v1.3；2、所有单项getElement切换到querySelector

--- EhTagBuilder.user.js
+++ EhTagBuilder.user.js
@@ -6,16 +6,16 @@
 // @description:zh-CN	从Wiki获取EhTagTranslater数据库，将E绅士TAG翻译为中文
 // @include     *://github.com/Mapaler/EhTagTranslator*
 // @icon        http://exhentai.org/favicon.ico
-// @version     2.7.0
+// @version     2.7.1
 // @grant       none
-// @copyright	2016+, Mapaler <mapaler@163.com>
+// @copyright	2017+, Mapaler <mapaler@163.com>
 // ==/UserScript==
 
 (function() {
 var wiki_URL="https://github.com/Mapaler/EhTagTranslator/wiki"; //GitHub wiki 的地址
 var rows_title="rows"; //行名的地址
-var buttonInserPlace = document.getElementsByClassName("pagehead-actions")[0]; //按钮插入位置
-var windowInserPlace = document.getElementsByClassName("reponav")[0]; //窗口插入位置
+var buttonInserPlace = document.querySelector(".pagehead-actions"); //按钮插入位置
+var windowInserPlace = document.querySelector(".reponav"); //窗口插入位置
 var scriptName = typeof(GM_info)!="undefined" ? (GM_info.script.localizedName ? GM_info.script.localizedName : GM_info.script.name) : "EhTagBuilder"; //本程序的名称
 var scriptVersion = typeof(GM_info)!="undefined" ? GM_info.script.version.replace(/(^\s*)|(\s*$)/g, "") : "LocalDebug"; //本程序的版本
 var optionVersion = 1; //当前设置版本，用于提醒是否需要重置设置
@@ -26,23 +26,30 @@ var rowsCurrent = 0; //当前下载行名
 //匹配Emoji正则表达式，Made by @xioxin
 var emojireg = /\u{2139}|[\u{2194}-\u{2199}]|[\u{21A9}-\u{21AA}]|[\u{231A}-\u{231B}]|\u{2328}|\u{23CF}|[\u{23E9}-\u{23F3}]|[\u{23F8}-\u{23FA}]|\u{24C2}|[\u{25AA}-\u{25AB}]|\u{25B6}|\u{25C0}|[\u{25FB}-\u{25FE}]|[\u{2600}-\u{2604}]|\u{260E}|\u{2611}|[\u{2614}-\u{2615}]|\u{2618}|\u{261D}|\u{2620}|[\u{2622}-\u{2623}]|\u{2626}|\u{262A}|[\u{262E}-\u{262F}]|[\u{2638}-\u{263A}]|[\u{2648}-\u{2653}]|\u{2660}|\u{2663}|\u{2666}|\u{2668}|\u{267B}|\u{267F}|[\u{2692}-\u{2697}]|\u{2699}|[\u{269B}-\u{269C}]|[\u{26A0}-\u{26A1}]|[\u{26AA}-\u{26AB}]|[\u{26B0}-\u{26B1}]|[\u{26BD}-\u{26BE}]|[\u{26C4}-\u{26C5}]|\u{26C8}|\u{26CE}|\u{26CF}|\u{26D1}|[\u{26D3}-\u{26D4}]|[\u{26E9}-\u{26EA}]|[\u{26F0}-\u{26F5}]|[\u{26F7}-\u{26FA}]|\u{26FD}|\u{2702}|\u{2705}|[\u{2708}-\u{2709}]|[\u{270A}-\u{270B}]|[\u{270C}-\u{270D}]|\u{270F}|\u{2712}|\u{2714}|\u{2716}|\u{271D}|\u{2721}|\u{2728}|[\u{2733}-\u{2734}]|\u{2744}|\u{2747}|\u{274C}|\u{274E}|[\u{2753}-\u{2755}]|\u{2757}|\u{2763}|[\u{2795}-\u{2797}]|\u{27A1}|\u{27B0}|\u{27BF}|[\u{2934}-\u{2935}]|[\u{2B05}-\u{2B07}]|[\u{2B1B}-\u{2B1C}]|\u{2B50}|\u{2B55}|\u{3030}|\u{303D}|\u{3297}|\u{3299}|\u{1F004}|\u{1F0CF}|[\u{1F170}-\u{1F171}]|\u{1F17E}|\u{1F17F}|\u{1F18E}|[\u{1F191}-\u{1F19A}]|[\u{1F1E6}-\u{1F1FF}]|[\u{1F201}-\u{1F202}]|\u{1F21A}|\u{1F22F}|[\u{1F232}-\u{1F23A}]|[\u{1F250}-\u{1F251}]|[\u{1F300}-\u{1F320}]|\u{1F321}|[\u{1F324}-\u{1F32C}]|[\u{1F32D}-\u{1F32F}]|[\u{1F330}-\u{1F335}]|\u{1F336}|[\u{1F337}-\u{1F37C}]|\u{1F37D}|[\u{1F37E}-\u{1F37F}]|[\u{1F380}-\u{1F393}]|[\u{1F396}-\u{1F397}]|[\u{1F399}-\u{1F39B}]|[\u{1F39E}-\u{1F39F}]|[\u{1F3A0}-\u{1F3C4}]|\u{1F3C5}|[\u{1F3C6}-\u{1F3CA}]|[\u{1F3CB}-\u{1F3CE}]|[\u{1F3CF}-\u{1F3D3}]|[\u{1F3D4}-\u{1F3DF}]|[\u{1F3E0}-\u{1F3F0}]|[\u{1F3F3}-\u{1F3F5}]|\u{1F3F7}|[\u{1F3F8}-\u{1F3FF}]|[\u{1F400}-\u{1F43E}]|\u{1F43F}|\u{1F440}|\u{1F441}|[\u{1F442}-\u{1F4F7}]|\u{1F4F8}|[\u{1F4F9}-\u{1F4FC}]|\u{1F4FD}|\u{1F4FF}|[\u{1F500}-\u{1F53D}]|[\u{1F549}-\u{1F54A}]|[\u{1F54B}-\u{1F54E}]|[\u{1F550}-\u{1F567}]|[\u{1F56F}-\u{1F570}]|[\u{1F573}-\u{1F579}]|\u{1F57A}|\u{1F587}|[\u{1F58A}-\u{1F58D}]|\u{1F590}|[\u{1F595}-\u{1F596}]|\u{1F5A4}|\u{1F5A5}|\u{1F5A8}|[\u{1F5B1}-\u{1F5B2}]|\u{1F5BC}|[\u{1F5C2}-\u{1F5C4}]|[\u{1F5D1}-\u{1F5D3}]|[\u{1F5DC}-\u{1F5DE}]|\u{1F5E1}|\u{1F5E3}|\u{1F5E8}|\u{1F5EF}|\u{1F5F3}|\u{1F5FA}|[\u{1F5FB}-\u{1F5FF}]|\u{1F600}|[\u{1F601}-\u{1F610}]|\u{1F611}|[\u{1F612}-\u{1F614}]|\u{1F615}|\u{1F616}|\u{1F617}|\u{1F618}|\u{1F619}|\u{1F61A}|\u{1F61B}|[\u{1F61C}-\u{1F61E}]|\u{1F61F}|[\u{1F620}-\u{1F625}]|[\u{1F626}-\u{1F627}]|[\u{1F628}-\u{1F62B}]|\u{1F62C}|\u{1F62D}|[\u{1F62E}-\u{1F62F}]|[\u{1F630}-\u{1F633}]|\u{1F634}|[\u{1F635}-\u{1F640}]|[\u{1F641}-\u{1F642}]|[\u{1F643}-\u{1F644}]|[\u{1F645}-\u{1F64F}]|[\u{1F680}-\u{1F6C5}]|[\u{1F6CB}-\u{1F6CF}]|\u{1F6D0}|[\u{1F6D1}-\u{1F6D2}]|[\u{1F6E0}-\u{1F6E5}]|\u{1F6E9}|[\u{1F6EB}-\u{1F6EC}]|\u{1F6F0}|\u{1F6F3}|[\u{1F6F4}-\u{1F6F6}]|[\u{1F6F7}-\u{1F6F8}]|[\u{1F910}-\u{1F918}]|[\u{1F919}-\u{1F91E}]|\u{1F91F}|[\u{1F920}-\u{1F927}]|[\u{1F928}-\u{1F92F}]|\u{1F930}|[\u{1F931}-\u{1F932}]|[\u{1F933}-\u{1F93A}]|[\u{1F93C}-\u{1F93E}]|[\u{1F940}-\u{1F945}]|[\u{1F947}-\u{1F94B}]|\u{1F94C}|[\u{1F950}-\u{1F95E}]|[\u{1F95F}-\u{1F96B}]|[\u{1F980}-\u{1F984}]|[\u{1F985}-\u{1F991}]|[\u{1F992}-\u{1F997}]|\u{1F9C0}|[\u{1F9D0}-\u{1F9E6}]/gu;
 
-//访GM_xmlhttpRequest函数v1.0
-if(typeof(GM_xmlhttpRequest) == "undefined")
-{
-	var GM_xmlhttpRequest = function(GM_param){
-		var xhr = new XMLHttpRequest();	//创建XMLHttpRequest对象
-		if(GM_param.responseType) xhr.responseType = GM_param.responseType;
-		xhr.onreadystatechange = function()  //设置回调函数
-		{
-			if (xhr.readyState == 4 && xhr.status == 200)
-			GM_param.onload(xhr);
-		}
-		for (var header in GM_param.headers){
-			xhr.setRequestHeader(header, GM_param.headers[header]);
-		}
-		xhr.open(GM_param.method, GM_param.url, true);
-		xhr.send(GM_param.data ? GM_param.data : null);
-	}
+//仿GM_xmlhttpRequest函数v1.3
+if (typeof(GM_xmlhttpRequest) == "undefined") {
+    var GM_xmlhttpRequest = function(GM_param) {
+
+        var xhr = new XMLHttpRequest(); //创建XMLHttpRequest对象
+        xhr.open(GM_param.method, GM_param.url, true);
+        if (GM_param.responseType) xhr.responseType = GM_param.responseType;
+        if (GM_param.overrideMimeType) xhr.overrideMimeType(GM_param.overrideMimeType);
+        xhr.onreadystatechange = function() //设置回调函数
+            {
+                if (xhr.readyState === xhr.DONE) {
+                    if (xhr.status === 200 && GM_param.onload)
+                        GM_param.onload(xhr);
+                    if (xhr.status !== 200 && GM_param.onerror)
+                        GM_param.onerror(xhr);
+                }
+            }
+
+        for (var header in GM_param.headers) {
+            xhr.setRequestHeader(header, GM_param.headers[header]);
+        }
+
+        xhr.send(GM_param.data ? GM_param.data : null);
+    }
 }
 //仿GM_getValue函数v1.0
 if(typeof(GM_getValue) == "undefined")
@@ -107,10 +114,10 @@ var rowObj = function(){
 				method: "GET",
 				url: wiki_URL + (rowObj.name.length?"/"+rowObj.name:""),
 				onload: function(response) {
-					var page_get_w = document.getElementById("ETB_page-get");
+					var page_get_w = document.querySelector("#ETB_page-get");
 					if (page_get_w)
 					{
-						var statetxt = page_get_w.getElementsByClassName("page-get-" + rowObj.name)[0];
+						var statetxt = page_get_w.querySelector(".page-get-" + rowObj.name);
 						statetxt.classList.add("page-load");
 						statetxt.innerHTML = "获取成功";
 					}
@@ -139,15 +146,15 @@ function dealRows(response, dataset)
 	var parser = new DOMParser();
 	PageDOM = parser.parseFromString(response, "text/html");
 	
-	var page_get_w = document.getElementById("ETB_page-get");
+	var page_get_w = document.querySelector("#ETB_page-get");
 	if (page_get_w)
 	{
-		var statetxt = page_get_w.getElementsByClassName("page-get-rows")[0];
+		var statetxt = page_get_w.querySelector(".page-get-rows");
 		statetxt.classList.add("page-load");
 		statetxt.innerHTML = "获取成功";
 	}
 				
-	var wiki_body = PageDOM.getElementById("wiki-body").getElementsByTagName("div")[0];
+	var wiki_body = PageDOM.querySelector("#wiki-body").querySelector("div");
 	var linksn = wiki_body.getElementsByTagName("a");
 	for (var ai=0, ailen=linksn.length; ai<ailen; ai++)
 	{
@@ -162,7 +169,7 @@ function dealRows(response, dataset)
 		}
 	}
 	
-	var table = wiki_body.getElementsByTagName("table")[0].tBodies[0];
+	var table = wiki_body.querySelector("table").tBodies[0];
 	rowsCount = table.rows.length;
 	for(var ri=0, rilen=table.rows.length; ri<rilen; ri++)
 	{
@@ -176,7 +183,7 @@ function dealRows(response, dataset)
 		
 		if (page_get_w)
 		{
-			page_get_w.getElementsByClassName("select-menu-list")[0].appendChild(
+			page_get_w.querySelector(".select-menu-list").appendChild(
 				buildMenuItem((function()    
 					{
 						var div = document.createElement("div");
@@ -296,8 +303,8 @@ function dealTags(response, rowdataset)
 	var parser = new DOMParser();
 	PageDOM = parser.parseFromString(response, "text/html");
 	
-	var wiki_body = PageDOM.getElementById("wiki-body").getElementsByTagName("div")[0];
-	var table = wiki_body.getElementsByTagName("table")[0].tBodies[0];
+	var wiki_body = PageDOM.querySelector("#wiki-body").querySelector("div");
+	var table = wiki_body.querySelector("table").tBodies[0];
 	
 	for(var ri=0, rilen=table.rows.length; ri<rilen; ri++)
 	{
@@ -350,7 +357,7 @@ function startProgram(dataset, mode){
 //创建下载进度窗口
 function buildDownloadProgress()
 {
-	var page_get_w = document.getElementById("ETB_page-get");
+	var page_get_w = document.querySelector("#ETB_page-get");
 	if (!page_get_w)
 	{
 		windowInserPlace.appendChild(buildMenuModal("window", "ETB_page-get", "数据获取进度", null, [
@@ -404,7 +411,7 @@ function startProgramCheck(dataset, mode)
 				buildCSSOutput(dataset);
 				break;
 		}
-		var page_get_w = document.getElementById("ETB_page-get");
+		var page_get_w = document.querySelector("#ETB_page-get");
 		if (page_get_w)
 		{
 			page_get_w.parentNode.removeChild(page_get_w);
@@ -429,7 +436,7 @@ function buildCSSOutput(dataset)
 	var downBlob = new Blob([css], {'type': 'text\/css'});
 	var downurl = window.URL.createObjectURL(downBlob);
 	
-	var css_output_w = document.getElementById("ETB_css-output");
+	var css_output_w = document.querySelector("#ETB_css-output");
 	if (!css_output_w)
 	{
 		windowInserPlace.appendChild(buildMenuModal("window", "ETB_css-output", "用户样式版EhTagTranslator", null,
@@ -463,8 +470,8 @@ function buildCSSOutput(dataset)
 	else
 	{
 		css_output_w.style.display = "block";
-		css_output_w.getElementsByClassName("ETB_css-textarea")[0].value = css;
-		css_output_w.getElementsByTagName("a")[0].href = downurl;
+		css_output_w.querySelector(".ETB_css-textarea").value = css;
+		css_output_w.querySelector("a").href = downurl;
 	}
 }
 
@@ -611,7 +618,7 @@ function buildJSOutput(dataset)
 	var downBlob = new Blob([json], {'type': 'text\/json'});
 	var downurl = window.URL.createObjectURL(downBlob);
 	
-	var js_output_w = document.getElementById("ETB_js-output");
+	var js_output_w = document.querySelector("#ETB_js-output");
 	if (!js_output_w)
 	{
 		windowInserPlace.appendChild(buildMenuModal("window", "ETB_js-output", "用户脚本版EhTagTranslator数据库", null,
@@ -644,8 +651,8 @@ function buildJSOutput(dataset)
 	else
 	{
 		js_output_w.style.display = "block";
-		js_output_w.getElementsByClassName("ETB_js-textarea")[0].value = json;
-		js_output_w.getElementsByTagName("a")[0].href = downurl;
+		js_output_w.querySelector(".ETB_js-textarea").value = json;
+		js_output_w.querySelector("a").href = downurl;
 	}
 }
 
@@ -989,7 +996,7 @@ function buildSVG(mode,check)
 		{
 			var idName = linearGradient[lGi].id;
 			var idIndex = 0;
-			while(document.getElementById(idName + "_" + idIndex.toString()))
+			while(document.querySelector("#" + idName + "_" + idIndex.toString()))
 			{
 				idIndex++;
 			}
@@ -1006,7 +1013,7 @@ function buildSVG(mode,check)
 //打开设置窗口
 function startOption()
 {
-	var option_modal_w = document.getElementById("ETB_option");
+	var option_modal_w = document.querySelector("#ETB_option");
 	if (!option_modal_w)
 	{
 		windowInserPlace.appendChild(
@@ -1225,7 +1232,7 @@ function visitChildNodes(dom, callback)
 //保存设置
 function saveOption()
 {
-	var option_modal_w = document.getElementById("ETB_option");
+	var option_modal_w = document.querySelector("#ETB_option");
 	if (option_modal_w)
 	{
 		 visitChildNodes(option_modal_w,setValue);
@@ -1245,7 +1252,7 @@ function saveOption()
 //重新加载设置窗口
 function reloadOption()
 {
-	var option_modal_w = document.getElementById("ETB_option");
+	var option_modal_w = document.querySelector("#ETB_option");
 	if (option_modal_w)
 	{
 		 visitChildNodes(option_modal_w, getValue);
@@ -1299,5 +1306,5 @@ var menu_modal = buildMenuModal("menu", null, "请选择任务 v" + scriptVersio
 		])
 	]);
 	
-buttonInserPlace.insertBefore(buildButton(" " + scriptName + " ", buildSVG("eh"), menu_modal),buttonInserPlace.getElementsByTagName("li")[0]);
+buttonInserPlace.insertBefore(buildButton(" " + scriptName + " ", buildSVG("eh"), menu_modal),buttonInserPlace.querySelector("li"));
 })();
