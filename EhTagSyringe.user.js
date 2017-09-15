6957e88d002a7037d283ce81c937cff666693026
xin <i@xioxin.com>
2017-09-16 02:09:58 +0800

    Aria2 可以使用啦 因为不是很懂这个

--- EhTagSyringe.user.js
+++ EhTagSyringe.user.js
@@ -9,9 +9,10 @@
 // @include     *://e-hentai.org/*
 // @connect     raw.githubusercontent.com
 // @connect     github.com
+// @connect     localhost
+// @connect     127.0.0.1
 // @icon        http://exhentai.org/favicon.ico
 // @require     https://cdn.bootcss.com/angular.js/1.4.6/angular.min.js
-// @require     https://greasyfork.org/scripts/5672-aria2-rpc/code/Aria2%20RPC.js?version=118265
 // @resource    template         https://raw.githubusercontent.com/Mapaler/EhTagTranslator/master/template/ets-builder-menu.html?v=15
 // @resource    ets-prompt       https://raw.githubusercontent.com/Mapaler/EhTagTranslator/master/template/ets-prompt.html?v=23
 // @resource    ui-translate     https://raw.githubusercontent.com/Mapaler/EhTagTranslator/master/template/ui-translate.css?v=3
@@ -32,6 +33,276 @@
 // @copyright	2017+, Mapaler <mapaler@163.com> , xioxin <i@xioxin.com>
 // ==/UserScript==
 
+
+var Aria2 = (function (_isGM, _arrFn, _merge, _format, _isFunction) {
+    var jsonrpc_ver = '2.0';
+
+    if (_isGM) {
+        var doRequest = function ( opts ) {
+            console.warn ([
+                'Warning: You are now using an simple implementation of GM_xmlhttpRequest',
+                'Cross-domain request are not avilible unless configured correctly @ target server.',
+                '',
+                'Some of its features are not avilible, such as `username` and `password` field.'
+            ].join('\n'));
+
+            var oReq = new XMLHttpRequest ();
+            var cbCommon = function (cb) {
+                return (function () {
+                    cb ({
+                        readyState: oReq.readyState,
+                        responseHeaders: opts.getHeader ? oReq.getAllResponseHeaders() : null,
+                        getHeader: oReq.getResponseHeader.bind (oReq),
+                        responseText: oReq.responseText,
+                        status: oReq.status,
+                        statusText: oReq.statusText
+                    });
+                }).bind (opts);
+            };
+
+            if (opts.onload)  oReq.onload   = cbCommon (opts.onload);
+            if (opts.onerror) oReq.onerror  = cbCommon (opts.onerror);
+
+            oReq.open(opts.method || 'GET', opts.url, !opts.synchronous);
+
+            if (opts.headers) {
+                Object.keys(opts.headers).forEach (function (key) {
+                    oReq.setRequestHeader (key, opts.headers[key]);
+                });
+            }
+            return oReq.send(opts.data || null);
+        };
+    } else {
+        var doRequest = GM_xmlhttpRequest;
+    }
+
+    var AriaBase = function ( options ) {
+        this.options = _merge ({
+            auth: {
+                type: AriaBase.AUTH.noAuth,
+                user: '',
+                pass: ''
+            },
+            host: '127.0.0.1',
+            port: 6800
+        }, options || {});
+
+        this.id = parseInt (options, 10) || (+ new Date());
+    };
+
+    // 静态常量
+    AriaBase.AUTH = {
+        noAuth: 0,
+        basic:  1,
+        secret: 2
+    };
+
+    // public 函数
+    AriaBase.prototype = {
+        getBasicAuth: function () {
+            return btoa (_format('%s:%s', this.options.auth.user, this.options.auth.pass));
+        },
+
+        send: function ( bIsDataBatch, data, cbSuccess, cbError ) {
+            var srcTaskObj = { jsonrpc: jsonrpc_ver, id: this.id };
+
+            var payload = {
+
+                method: 'POST',
+                url: _format('http://%s:%s/jsonrpc', this.options.host, this.options.port),
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                },
+                data: bIsDataBatch
+                    ? data.map (function (e) { return _merge ({}, srcTaskObj, e); })
+                    : _merge ({}, srcTaskObj, data),
+                onload: function (r) {
+                    var repData = JSON.parse (r.responseText);
+                    if (repData.error) {
+                        cbError && cbError (false, repData);
+                    } else {
+                        cbSuccess && cbSuccess (repData);
+                    }
+                },
+                onerror: cbError ? cbError.bind(null, false) : null
+            };
+
+            switch ( parseInt (this.options.auth.type, 10) ) {
+                case AriaBase.AUTH.noAuth:
+                    // DO NOTHING
+                    break;
+
+                case AriaBase.AUTH.basic:
+                    payload.headers.Authorization = 'Basic ' + this.getBasicAuth();
+                    break;
+
+                case AriaBase.AUTH.secret:
+                    (function (sToken) {
+                        if (bIsDataBatch) {
+                            for (var i = 0; i < payload.data.length; i++) {
+                                payload.data[i].params.splice(0, 0, sToken);
+                            }
+                        } else {
+                            if (!payload.data.params)
+                                payload.data.params = [];
+                            payload.data.params.splice(0, 0, sToken);
+                        }
+                    })(_format('token:%s', this.options.auth.pass));
+                    break;
+
+                default:
+                    throw new Error('Undefined auth type: ' + this.options.auth.type);
+            }
+
+            payload.data = JSON.stringify ( payload.data );
+
+            return doRequest (payload);
+        },
+
+        // batchAddUri ( foo, { uri: 'http://example.com/xxx', options: { ... } } )
+        batchAddUri: function (fCallback) {
+            console.warn (
+                'This function [%s] has deprecated! Consider use %s instead.',
+                'batchAddUri', 'AriaBase.BATCH'
+            );
+
+            // { url, name }
+            var payload = [].slice.call (arguments, 1).map (function (arg) {
+                return {
+                    method: 'aria2.addUri',
+                    params: [ arg.uri.map ? arg.uri : [ arg.uri ] ].concat (arg.options || [])
+                };
+            });
+
+            return this.send (true, payload, fCallback, fCallback);
+        }
+    };
+
+
+    // 添加各类函数
+    AriaBase.fn = {};
+    _arrFn.forEach (function (sMethod) {
+        // 函数链表
+        AriaBase.fn[sMethod] = sMethod;
+
+        // arg1, arg2, ... , [cbSuccess, [cbError]]
+        AriaBase.prototype[sMethod] = function ( ) {
+            var args = [].slice.call (arguments);
+
+            var cbSuccess, cbError;
+            if (args.length && _isFunction(args[args.length - 1])) {
+                cbSuccess = args[args.length - 1];
+                args.splice (-1, 1);
+
+                if (args.length && _isFunction(args[args.length - 1])) {
+                    cbError = cbSuccess;
+                    cbSuccess = args[args.length - 1];
+                    args.splice (-1, 1);
+                }
+            }
+
+            return this.send (false, {
+                method: 'aria2.' + sMethod,
+                params: args
+            }, cbSuccess, cbError);
+        };
+    });
+
+    AriaBase.BATCH = function ( parent, cbSuccess, cbFail ) {
+        if (!(parent instanceof AriaBase))
+            throw new Error ('Parent is not AriaBase!');
+
+        this.parent = parent;
+        this.data = [];
+
+        this.onSuccess = cbSuccess;
+        this.onFail = cbFail;
+    };
+
+    AriaBase.BATCH.prototype = {
+        addRaw: function (fn, args) {
+            this.data.push ({
+                method: 'aria2.' + fn,
+                params: args
+            });
+            return this;
+        },
+
+        add: function (fn) {
+            // People can add more without edit source.
+            if (!AriaBase.fn[fn])
+                throw new Error ('Unknown function: ' + fn + ', please check if you had a typo.');
+
+            return this.addRaw (fn, [].slice.call(arguments, 1));
+        },
+
+        send: function () {
+            // bIsDataBatch, data, cbSuccess, cbError
+            var ret = this.parent.send ( true, this.data, this.onSuccess, this.onFail );
+            this.reset ();
+            return ret;
+        },
+
+        getActions: function () {
+            return this.data.slice();
+        },
+
+        setActions: function (actions) {
+            if (!actions || !actions.map) return ;
+
+            this.data = actions;
+        },
+
+        reset: function () {
+            this.onSuccess = this.onFail = null;
+            this.setActions ( [] );
+        }
+    };
+
+    return AriaBase;
+})
+// const 变量
+('undefined' == typeof GM_xmlhttpRequest, [
+        "addUri", "addTorrent", "addMetalink", "remove", "forceRemove",
+        "pause", "pauseAll", "forcePause", "forcePauseAll", "unpause",
+        "unpauseAll", "tellStatus", "getUris", "getFiles", "getPeers",
+        "getServers", "tellActive", "tellWaiting", "tellStopped",
+        "changePosition", "changeUri", "getOption", "changeOption",
+        "getGlobalOption", "changeGlobalOption", "getGlobalStat",
+        "purgeDownloadResult", "removeDownloadResult", "getVersion",
+        "getSessionInfo", "shutdown", "forceShutdown", "saveSession"
+    ],
+// private 函数
+    (function (base) {
+        var _isObject = function (obj) {
+            return obj instanceof Object;
+        };
+        var _merge = function (base) {
+            var args = arguments,
+                argL = args.length;
+            for ( var i = 1; i < argL; i++ ) {
+                Object.keys (args[i]).forEach (function (key) {
+                    if (_isObject(args[i][key]) && _isObject(base[key])) {
+                        base[key] = _merge (base[key], args[i][key]);
+                    } else {
+                        base[key] = args[i][key];
+                    }
+                });
+            }
+            return base;
+        };
+        return _merge;
+    })(), function (src) {
+        var args = arguments,
+            argL = args.length;
+
+        var ret = src.slice ();
+        for ( var i = 1; i < argL; i++ )
+            ret = ret.replace ('%s', args[i]);
+        return ret;
+    }, function (foo) {
+        return typeof foo === 'function'
+    });
 
 
 (function() {
@@ -94,7 +365,7 @@
                 user: '',
                 pass: ''
             },
-            host: '127.0.0.1',
+            host: 'localhost',
             port: 6800
         },
         'style':{
@@ -201,10 +472,6 @@ div.gtl{
 
     console.log('ets config:',etbConfig);
 
-    let ariaOptions = JSON.parse(JSON.stringify(etbConfig.ariaOptions));
-    ariaOptions.auth.type *= 1;
-    unsafeWindow.aria = new Aria2(ariaOptions);
-    console.log('unsafeWindow.aria',unsafeWindow.aria);
 
     function EhTagUITranslator(){
 
@@ -1147,11 +1414,49 @@ div.gtl{
     }
 
     //小米路由下载助手
-    function EhTagMiWifi(){
-        if(!(/gallerytorrents\.php/).test(unsafeWindow.location.href)){
+    function EhTagMiWifi() {
+        if (!(/gallerytorrents\.php/).test(unsafeWindow.location.href)) {
             return;
         }
         console.log('EhTagMiWifi');
+        let tableList = document.querySelectorAll("#torrentinfo form table");
+        if (tableList && tableList.length) tableList.forEach(function (table) {
+            let href = '';
+            let a    = table.querySelector('a');
+            if (a) href = a.href;
+            if (!href) return;
+
+            let magnet = href.replace(/.*?([0-9a-f]{40}).*$/i, "magnet:?xt=urn:btih:$1");
+            if (magnet.length != 60) return;
+
+            let insertionPoint = table.querySelector('input');
+            if (!insertionPoint) return;
+
+            var button       = document.createElement("input");
+            button.type      = "button";
+            button.value     = "下载到小米路由器";
+            button.className = 'stdbtn';
+            button.onclick   = function () {
+                unsafeWindow.resizeTo(1000, 600);
+                unsafeWindow.location.href = `https://d.miwifi.com/d2r/?url=${btoa(magnet)}`;
+            };
+            insertionPoint.parentNode.insertBefore(button, insertionPoint);
+        })
+
+    }
+    function EhTagAria2Helper(){
+        if(!(/gallerytorrents\.php/).test(unsafeWindow.location.href)){
+            return;
+        }
+
+
+        let ariaOptions = JSON.parse(JSON.stringify(etbConfig.ariaOptions));
+        ariaOptions.auth.type *= 1;
+        let aria = new Aria2(ariaOptions);
+        console.log('unsafeWindow.aria',unsafeWindow.aria);
+
+
+        console.log('EhTagAria2Helper');
         let tableList = document.querySelectorAll("#torrentinfo form table");
         if(tableList&&tableList.length)tableList.forEach(function (table) {
             let href = '';
@@ -1159,23 +1464,26 @@ div.gtl{
             if(a)href = a.href;
             if(!href)return;
 
-            let magnet = href.replace(/.*?([0-9a-f]{40}).*$/i,"magnet:?xt=urn:btih:$1") ;
-            if(magnet.length != 60)return;
-
             let insertionPoint = table.querySelector('input');
             if(!insertionPoint)return;
 
             var button = document.createElement("input");
             button.type = "button";
-            button.value = "下载到小米路由器";
+            button.value = "Aria2下载";
             button.className = 'stdbtn';
             button.onclick = function () {
-                unsafeWindow.resizeTo(1000, 600);
-                unsafeWindow.location.href =`https://d.miwifi.com/d2r/?url=${btoa(magnet)}`;
+                aria.addUri([href],function(){
+                    myNotification('添加成功',{
+                        body:href
+                    });
+                },function () {
+                    myNotification('添加失败',{
+                        body:href
+                    });
+                });
             };
             insertionPoint.parentNode.insertBefore( button, insertionPoint );
         })
-
 
 
 
@@ -1590,6 +1898,7 @@ ${css}
             if(etbConfig.searchHelper)EhTagInputHelper();
             if(etbConfig.download2miwifi)EhTagMiWifi();
             // EhTagMiWifi();
+            if(etbConfig.ariaHelper)EhTagAria2Helper();
             if(etbConfig.magnetHelper)EhTagMagnetHelper();
             if(etbConfig.UITranslate)EhTagUITranslator();
         }
